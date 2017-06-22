ac97a949b5e8690d7c66451a7a30577638d63e32
Michael Keyser <Erutan409@users.noreply.github.com>
2017-06-21 23:16:05 -0400

    Add files via upload

---
+++ Relay.cpp
@@ -0,0 +1,125 @@
+#include <Arduino.h>
+#include <Relay.h>
+
+Relay::Relay(RELAY_SIZE size) {
+    this->_size = size;
+}
+
+// instructs the object that it's ready to do work,
+// but first checks to make sure everything is
+// properly configured first
+bool Relay::initialize(void) {
+
+    for (int i = 0; i < this->getSize(); i++) {
+
+        if (this->_pins[i] == NULL) {
+            return false;
+        }
+
+    }
+
+    this->_initialized = true;
+    this->commit();
+    return this->_initialized;
+
+}
+
+RELAY_SIZE Relay::getSize(void) {
+    return this->_size;
+}
+
+Relay &Relay::setPin(int pinNumber, RELAY_NUM relay) {
+
+    // don't allow any pin setting if alread initialized
+    if (this->_initialized || this->_translateRelayNumber(relay) > this->getSize()) {goto done;}
+
+    // ensure the pin mode is set for output
+    pinMode(pinNumber,OUTPUT);
+    //digitalWrite(pinNumber,HIGH);
+    this->_pins[(this->_translateRelayNumber(relay) - 1)] = pinNumber;
+    // set default state for pin
+    this->_futuePinState[(this->_translateRelayNumber(relay) - 1)] = HIGH_R;
+
+    done:
+    return *this;
+
+}
+
+int *Relay::getPin(RELAY_NUM relay) {
+    return &this->_pins[(this->_translateRelayNumber(relay) - 1)];
+}
+
+Relay &Relay::on(RELAY_NUM relay) {
+    return this->_toggle(relay, LOW_R);
+}
+
+Relay &Relay::off(RELAY_NUM relay) {
+    return this->_toggle(relay, HIGH_R);
+}
+
+RELAY_STATE *Relay::getState(RELAY_NUM relay) {
+    return &this->_currentPinState[(this->_translateRelayNumber(relay) - 1)];
+}
+
+Relay &Relay::_toggle(RELAY_NUM relay, RELAY_STATE state) {
+
+    if (!this->_initialized || this->_translateRelayNumber(relay) > this->getSize()) {goto done;}
+
+    this->_futuePinState[(this->_translateRelayNumber(relay) - 1)] = state;
+
+    done:
+    return *this;
+
+}
+
+void Relay::commit(void) {
+
+    if (!this->_initialized) {goto done;}
+
+    for (int i = 0; i < this->getSize(); i++) {
+
+        RELAY_STATE &c = this->_currentPinState[i];
+        RELAY_STATE &f = this->_futuePinState[i];
+
+        if (c != f) {
+
+            c = f;
+            digitalWrite(this->_pins[i],(c == HIGH_R? HIGH : LOW));
+
+        }
+
+    }
+
+    done:
+    return;
+
+}
+
+int Relay::_translateRelayNumber(RELAY_NUM relay) {
+
+    int num;
+
+    switch (relay) {
+
+        case FIRST_R: num = 1; break;
+        case SECOND_R: num = 2; break;
+        case THIRD_R: num = 3; break;
+        case FOURTH_R: num = 4; break;
+        case FIFTH_R: num = 5; break;
+        case SIXTH_R: num = 6; break;
+        case SEVENTH_R: num = 7; break;
+        case EIGHTH_R: num = 8; break;
+        case NINTH_R: num = 9; break;
+        case TENTH_R: num = 10; break;
+        case ELEVENTH_R: num = 11; break;
+        case TWELFTH_R: num = 12; break;
+        case THIRTEENTH_R: num = 13; break;
+        case FOURTEENTH_R: num = 14; break;
+        case FIFTEENTH_R: num = 15; break;
+        case SIXTEENTH_R: num = 16; break;
+
+    }
+
+    return num;
+
+}
---
+++ Relay.h
@@ -0,0 +1,62 @@
+#ifndef RELAY_H
+#define RELAY_H
+
+#include <Arduino.h>
+
+enum RELAY_SIZE {
+    ONE_R       = 0x01
+    ,TWO_R      = 0x02
+    ,FOUR_R     = 0x04
+    ,EIGHT_R    = 0x08
+    ,SIXTEEN_R  = 0x10
+};
+
+enum RELAY_NUM {
+    FIRST_R         = 0x02
+    ,SECOND_R       = 0x04
+    ,THIRD_R        = 0x08
+    ,FOURTH_R       = 0x10
+    ,FIFTH_R        = 0x20
+    ,SIXTH_R        = 0x40
+    ,SEVENTH_R      = 0x80
+    ,EIGHTH_R       = 0x100
+    ,NINTH_R        = 0x200
+    ,TENTH_R        = 0x400
+    ,ELEVENTH_R     = 0x800
+    ,TWELFTH_R      = 0x1000
+    ,THIRTEENTH_R   = 0x2000
+    ,FOURTEENTH_R   = 0x4000
+    ,FIFTEENTH_R    = 0x8000
+    ,SIXTEENTH_R    = 0x10000
+};
+
+enum RELAY_STATE {
+    LOW_R       = 0
+    ,HIGH_R     = 1
+};
+
+class Relay {
+
+    public:
+        Relay(RELAY_SIZE size);
+        bool initialize(void);
+        RELAY_SIZE getSize(void);
+        Relay &setPin(int pinNumber, RELAY_NUM relay);
+        int *getPin(RELAY_NUM rela);
+        Relay &on(RELAY_NUM relay);
+        Relay &off(RELAY_NUM relay);
+        void commit(void);
+        RELAY_STATE *getState(RELAY_NUM relay);
+
+    private:
+        bool _initialized = false;
+        RELAY_SIZE _size;
+        int _pins[16];
+        RELAY_STATE _currentPinState[16];
+        RELAY_STATE _futuePinState[16];
+        int _translateRelayNumber(RELAY_NUM relay);
+        Relay &_toggle(RELAY_NUM relay, RELAY_STATE state);
+
+};
+
+#endif
