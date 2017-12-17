a5c7f14d0e09a1a3fccdedac7a6289ac4da83923
Michael Keyser <Erutan409@users.noreply.github.com>
2017-12-16 20:27:33 -0500

    Fleshed out changes

--- Relay.cpp
+++ Relay.cpp
@@ -1,135 +1,71 @@
 #include <Arduino.h>
 #include <Relay.h>
+#include <Avail.h>
 
-#ifdef RelaySafety
+Relay::Relay(RELAY_SIZE size) {
+	this->_size = size;
+}
 
-	Relay::Relay(RELAY_SIZE size) {
-		this->_size = size;
+RELAY_SIZE *Relay::getSize(void) {
+	return &this->_size;
+}
+
+Relay &Relay::setPin(unsigned int pin, unsigned int relay) {
+	this->_pins[relay - 1] = pin;
+	pinMode(pin, OUTPUT);
+	digitalWrite(pin, HIGH);
+	return *this;
+}
+
+int Relay::getRelayByPin(unsigned int pin) {
+	for (unsigned short int i = 0; i < *this->getSize(); i++) {
+		if (this->_pins[i] == pin) { return i + 1; }
 	}
+	return 0;
+}
 
-	RELAY_SIZE *Relay::getSize(void) {
-		return &this->_size;
+int Relay::getPinByRelay(unsigned int relay) {
+	return &this->_pins[relay - 1];
+}
+
+Relay &Relay::on(unsigned int relay) {
+	this->_futurePinState[relay - 1] = RELAY_ON;
+	return *this;
+}
+
+Relay &Relay::off(unsigned int relay) {
+	this->_futurePinState[relay - 1] = RELAY_OFF;
+	return *this;
+}
+
+Relay &Relay::toggle(unsigned int relay) {
+	if (*this->getState(relay) == RELAY_OFF) {
+		return this->on(relay);
+	} else {
+		return this->off(relay);
 	}
+}
 
-	Relay &Relay::setPin(unsigned int pin, unsigned int relay) {
-		this->_pins[relay - 1] = pin;
-		pinMode(pin, OUTPUT);
-		digitalWrite(pin, HIGH);
-		return *this;
-	}
+Relay &Relay::commit(void) {
+	for (unsigned short int i = 0; i < *this->getSize(); i++) {
+		RELAY_STATE &c = this->_currentPinState[i];
+		RELAY_STATE &f = this->_futurePinState[i];
+		unsigned long *l = &this->_lastToggle[i];
 
-	int Relay::getRelayByPin(unsigned int pin) {
-		for (unsigned short int i = 0; i < *this->getSize(); i++) {
-			if (this->_pins[i] == pin) { return i + 1; }
-		}
-		return 0;
-	}
-
-	int Relay::getPinByRelay(unsigned int relay) {
-		return &this->_pins[relay - 1];
-	}
-
-	Relay &Relay::on(unsigned int relay) {
-		this->_futurePinState[relay - 1] = RELAY_ON;
-		return *this;
-	}
-
-	Relay &Relay::off(unsigned int relay) {
-		this->_futurePinState[relay - 1] = RELAY_OFF;
-		return *this;
-	}
-
-	void Relay::commit(void) {
-		for (unsigned short int i = 0; i < *this->getSize(); i++) {
-			RELAY_STATE &c = this->_currentPinState[i];
-			RELAY_STATE &f = this->_futurePinState[i];
-			unsigned long &l = this->_lastToggle[i];
-
-			if (c != f && this->_check(l)) {
-				c = f;
-				digitalWrite(this->_pins[i], (c == RELAY_ON ? LOW : HIGH));
-			}
-		}
-	}
-
-	RELAY_STATE *Relay::getState(unsigned int relay) {
-		return &this->_currentPinState[relay - 1];
-	}
-
-	unsigned long Relay::_getSafety(void) {
-		static unsigned short safety = nullptr;
-		if (safety == nullptr) { safety = max(atoi(RelaySafety), 166); /* in milliseconds */ }
-
-		return safety;
-	}
-
-	bool Relay::_check(unsigned long &lastToggle) {
-		unsigned long millis = millis();
-		unsigned long diff = millis - lastToggle;
-		bool rollover = diff > millis;
-
-		if (rollover) { // rollover
-			lastToggle = 4294967295 - lastToggle;
-			return false;
-		} else if (diff == 0 || diff > this->_getSafety()) {
-			return true;
-		} else {
-			return false;
+		if (c != f && avail(&this->_safety, l) == true) {
+			c = f;
+			*l = millis();
+			digitalWrite(this->_pins[i], (c == RELAY_ON ? LOW : HIGH));
 		}
 	}
 
-#else
+	return *this;
+}
 
-	Relay::Relay(RELAY_SIZE size) {
-		this->_size = size;
-	}
+RELAY_STATE *Relay::getState(unsigned int relay) {
+	return &this->_currentPinState[relay - 1];
+}
 
-	RELAY_SIZE *Relay::getSize(void) {
-		return &this->_size;
-	}
-
-	Relay &Relay::setPin(unsigned int pin, unsigned int relay) {
-		this->_pins[relay - 1] = pin;
-		pinMode(pin, OUTPUT);
-		digitalWrite(pin, HIGH);
-		return *this;
-	}
-
-	int Relay::getRelayByPin(unsigned int pin) {
-		for (unsigned short int i = 0; i < *this->getSize(); i++) {
-			if (this->_pins[i] == pin) { return i + 1; }
-		}
-		return 0;
-	}
-
-	int Relay::getPinByRelay(unsigned int relay) {
-		return &this->_pins[relay - 1];
-	}
-
-	Relay &Relay::on(unsigned int relay) {
-		this->_futurePinState[relay - 1] = RELAY_ON;
-		return *this;
-	}
-
-	Relay &Relay::off(unsigned int relay) {
-		this->_futurePinState[relay - 1] = RELAY_OFF;
-		return *this;
-	}
-
-	void Relay::commit(void) {
-		for (unsigned short int i = 0; i < *this->getSize(); i++) {
-			RELAY_STATE &c = this->_currentPinState[i];
-			RELAY_STATE &f = this->_futurePinState[i];
-
-			if (c != f) {
-				c = f;
-				digitalWrite(this->_pins[i], (c == RELAY_ON ? LOW : HIGH));
-			}
-		}
-	}
-
-	RELAY_STATE *Relay::getState(unsigned int relay) {
-		return &this->_currentPinState[relay - 1];
-	}
-
-#endif // RelaySafety
+Relay &Relay::setSafety(unsigned long safety) {
+	this->_safety = max(safety, 250);
+}
--- Relay.h
+++ Relay.h
@@ -16,55 +16,29 @@ enum RELAY_STATE {
     ,RELAY_OFF = 0x00
 };
 
-#ifdef RelaySafety
+class Relay {
 
-	class Relay {
+	public:
+		Relay(RELAY_SIZE size);
+		RELAY_SIZE *getSize(void);
+		Relay &setPin(unsigned int pin, unsigned int relay);
+		int getRelayByPin(unsigned int pin);
+		int getPinByRelay(unsigned int relay);
+		Relay &on(unsigned int relay);
+		Relay &off(unsigned int relay);
+		Relay &toggle(unsigned int relay);
+		Relay &commit(void);
+		RELAY_STATE *getState(unsigned int relay);
+		Relay &setSafety(unsigned long safety);
 
-		public:
-			Relay(RELAY_SIZE size);
-			RELAY_SIZE *getSize(void);
-			Relay &setPin(unsigned int pin, unsigned int relay);
-			int getRelayByPin(unsigned int pin);
-			int getPinByRelay(unsigned int relay);
-			Relay &on(unsigned int relay);
-			Relay &off(unsigned int relay);
-			void commit(void);
-			RELAY_STATE *getState(unsigned int relay);
+	private:
+		RELAY_SIZE _size;
+		int _pins[16] = { 0 };
+		RELAY_STATE _currentPinState[16] = { RELAY_OFF };
+		RELAY_STATE _futurePinState[16] = { RELAY_OFF };
+		unsigned long _lastToggle[16] = { 0 };
+		unsigned long _safety = 250;
 
-		private:
-			RELAY_SIZE _size;
-			int _pins[16] = { 0 };
-			RELAY_STATE _currentPinState[16] = { RELAY_OFF };
-			RELAY_STATE _futurePinState[16] = { RELAY_OFF };
-			unsigned long _lastToggle[16] = { 0 };
-			unsigned long _getSafety(void);
-			bool _check(unsigned int relay);
-
-	};
-
-#else
-
-	class Relay {
-
-		public:
-			Relay(RELAY_SIZE size);
-			RELAY_SIZE *getSize(void);
-			Relay &setPin(unsigned int pin, unsigned int relay);
-			int getRelayByPin(unsigned int pin);
-			int getPinByRelay(unsigned int relay);
-			Relay &on(unsigned int relay);
-			Relay &off(unsigned int relay);
-			void commit(void);
-			RELAY_STATE *getState(unsigned int relay);
-
-		private:
-			RELAY_SIZE _size;
-			int _pins[16] = { 0 };
-			RELAY_STATE _currentPinState[16] = { RELAY_OFF };
-			RELAY_STATE _futurePinState[16] = { RELAY_OFF };
-
-	};
-
-#endif // RelaySafety
+};
 
 #endif
