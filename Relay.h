04362d2d5c806d35220981d38f11048c43fbe222
Michael Keyser <Erutan409@users.noreply.github.com>
2018-06-17 00:04:00 -0400

    Refactor * Changed construct from a one class system controlling multiple modules to a simplified class for each individual module.

--- Relay.h
+++ Relay.h
@@ -3,41 +3,26 @@
 
 #include <Arduino.h>
 
-enum RELAY_SIZE {
-    ONE_RELAY      = 0x01
-    ,TWO_RELAY     = 0x02
-    ,FOUR_RELAY    = 0x04
-    ,EIGHT_RELAY   = 0x08
-    ,SIXTEEN_RELAY = 0x10
-};
-
 enum RELAY_STATE {
-	RELAY_ON   = 0x01
-    ,RELAY_OFF = 0x00
+	RELAY_ON   = 0x01,
+	RELAY_OFF = 0x00
 };
 
 class Relay {
 
 	public:
-		Relay(RELAY_SIZE size);
-		RELAY_SIZE *getSize(void);
-		Relay &setPin(uint8_t pin, uint8_t relay);
-		uint8_t getRelayByPin(uint8_t pin);
-		uint8_t getPinByRelay(uint8_t relay);
-		Relay &on(uint8_t relay);
-		Relay &off(uint8_t relay);
-		Relay &toggle(uint8_t relay);
-		Relay &commit(void);
-		RELAY_STATE *getState(uint8_t relay);
+		Relay(uint8_t pin);
+		Relay &on(void);
+		Relay &off(void);
+		Relay &toggle(void);
+		RELAY_STATE *getState(void);
 		Relay &setSafety(uint32_t safety);
 
 	private:
-		RELAY_SIZE _size;
-		uint8_t _pins[16] = { 0 };
-		RELAY_STATE _currentPinState[16] = { RELAY_OFF };
-		RELAY_STATE _futurePinState[16] = { RELAY_OFF };
-		uint32_t _lastToggle[16] = { 0 };
+		uint8_t _pin;
+		RELAY_STATE _currentState = RELAY_OFF;
 		uint32_t _safety = 250;
+		uint32_t _lastToggle = 0;
 
 };
 
